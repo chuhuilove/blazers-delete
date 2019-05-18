69509425cdebd7c4b6a3d7339f8f8579d2beb1c4
chu-hui-love <lovehui131125@gmail.com>
2019-05-18 10:09:24 +0800

    socket init

---
+++ socket/src/main/java/com/chuhui/blazers/socket/SocketExample.java
@@ -0,0 +1,20 @@
+package com.chuhui.blazers.socket;
+
+/**
+ * java  socket 示例
+ *
+ * 模拟tcp粘包与拆包
+ *
+ * @author: 纯阳子
+ * @Date: 2019/5/18
+ * @Description:TODO
+ */
+public class SocketExample {
+
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
