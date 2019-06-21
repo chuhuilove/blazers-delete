11dc7a421a19358b3a03ece6f2d7d725e98c74fb
chuhui <lovehui131125@gmail.com>
2019-06-21 16:56:37 +0800

    research lock

--- concurrent/src/main/java/com/chuhui/blazers/concurrent/aqs/AQSLock.java
+++ concurrent/src/main/java/com/chuhui/blazers/concurrent/aqs/AQSLock.java
@@ -1,6 +1,7 @@
 package com.chuhui.blazers.concurrent.aqs;
 
 import java.rmi.ServerError;
+import java.util.Arrays;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.ThreadPoolExecutor;
@@ -23,6 +24,9 @@ public class AQSLock {
 
     private volatile int count = 0;
 
+    ThreadPoolExecutor executor = null;
+
+
     public static void main(String[] args) {
 
         ThreadFactory factory = new ThreadFactory() {
@@ -33,41 +37,54 @@ public class AQSLock {
             public Thread newThread(Runnable r) {
 
                 Thread thread = new Thread(r);
-                thread.setName(String.format("AQSLock threadName %d", atomicInt.incrementAndGet()));
+                thread.setName(String.format("AQSLock threadName%d", atomicInt.incrementAndGet()));
 
                 return thread;
             }
         };
 
+        AQSLock aqsLock = new AQSLock();
 
-        ThreadPoolExecutor executor = new ThreadPoolExecutor(30, 30,
+        aqsLock.executor = new ThreadPoolExecutor(30, 100,
                 2000L, TimeUnit.MINUTES,
                 new ArrayBlockingQueue<>(100), factory);
-        AQSLock aqsLock = new AQSLock();
-        IntStream.rangeClosed(1, 20).forEach(e -> executor.execute(() -> aqsLock.lockAndInvoke()));
+
+        IntStream.rangeClosed(1, 20).forEach(e -> aqsLock.executor.execute(() -> aqsLock.lockAndInvoke()));
 
 
-        System.err.println("之后:" + aqsLock.count);
-        System.err.println("");
-        while (executor.isTerminated()) {
-        }
-        System.err.println("之后:" + aqsLock.count);
     }
 
 
     void lockAndInvoke() {
 
+
         /**
          * 每一个线程被封装成一个Node,这些Node会被组建成一个队列,
          *
          */
+        String currentThreadName = Thread.currentThread().getName();
+        System.err.println(currentThreadName + " 排队中");
         lock.lock();
+
+        //查找一下,是怎么入队的
+
+        System.err.println(currentThreadName + " 获取到锁了");
         try {
 
             int i = 100;
             while (i-- > 0) {
-                Thread.sleep(2000L);
-                System.err.println("当前线程:" + Thread.currentThread().getName());
+                Thread.sleep(100L);
+                if (i == 99) {
+                    System.err.println("当前线程正在运行线程:" + currentThreadName);
+                }
+
+
+                // 如果当前线程是tableName4,则再添加一个线程
+                if (currentThreadName.split(" ")[1].equals("threadName4") && i == 99) {
+
+                    executor.execute(() -> lockAndInvoke());
+                }
+
             }
 
         } catch (InterruptedException e) {
@@ -78,5 +95,6 @@ public class AQSLock {
 
 
     }
+
 
 }
