6da8b9d942677ad84d43dfbf83c8379efb84ef36
chuhui <lovehui131125@gmail.com>
2019-08-22 19:04:17 +0800

    多线程下,测试Atomic变量的安全性

--- concurrent/src/main/java/com/chuhui/blazers/concurrent/atomicNoSafe/Run.java
+++ concurrent/src/main/java/com/chuhui/blazers/concurrent/atomicNoSafe/Run.java
@@ -1,5 +1,11 @@
 package com.chuhui.blazers.concurrent.atomicNoSafe;
 
+import com.chuhui.blazers.commcustome.CustomerThreadFactory;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
 /**
  * Run
  *
@@ -13,26 +19,53 @@ public class Run {
      * 原子类也不安全?
      */
 
-    static final int THREAD_MAX_NUM = Runtime.getRuntime().availableProcessors()<<2;
+    static final int THREAD_MAX_NUM = Runtime.getRuntime().availableProcessors() << 2;
 
     public static void main(String[] args) throws InterruptedException {
 
-        MyService service = new MyService();
+        final MyService service = new MyService();
 
-        MyThread[] threadArr = new MyThread[THREAD_MAX_NUM];
+
+        // 创建一个线程池
+        // 核心只有1个,最大有1000
+        // 使用的是有界队列
+        // 当第二个任务进来以后,
+
+        ThreadPoolExecutor executor = new ThreadPoolExecutor(1, 1000, 1000L, TimeUnit.MILLISECONDS,
+                new ArrayBlockingQueue<>(2), new CustomerThreadFactory("atomicNoSafe:"));
 
 
         for (int i = 0; i < THREAD_MAX_NUM; i++) {
-            threadArr[i] = new MyThread(service);
+            executor.execute(() -> {
+                service.addNum();
+            });
+        }
+
+        executor.shutdown();
+
+
+        while (!executor.isTerminated()){
+
         }
 
 
-        for (int i = 0; i < THREAD_MAX_NUM; i++) {
-            threadArr[i].start();
-        }
 
 
-        System.err.println("the last output is:" + service.aiRef.get());
+//
+//        MyThread[] threadArr = new MyThread[THREAD_MAX_NUM];
+//
+//
+//        for (int i = 0; i < THREAD_MAX_NUM; i++) {
+//            threadArr[i] = new MyThread(service);
+//        }
+//
+//
+//        for (int i = 0; i < THREAD_MAX_NUM; i++) {
+//            threadArr[i].start();
+//        }
+//
+//
+        System.err.println("the last output is:" + MyService.aiRef.get());
 
 
     }
