6d40fe971617abcdcc1278e618848a0c829e3ae6
chuhui <lovehui131125@gmail.com>
2019-09-25 18:55:58 +0800

    JDK动态代理分析-完成

--- dynamic-proxy/src/test/java/com/chuhui/blazers/dyproxy/dynamicproxy/DynamicProxyServiceTest.java
+++ dynamic-proxy/src/test/java/com/chuhui/blazers/dyproxy/dynamicproxy/DynamicProxyServiceTest.java
@@ -6,9 +6,14 @@ import com.chuhui.blazers.dyproxy.dynamicproxy.jdkdynamicproxy.JdkDynamicProxy.S
 import com.chuhui.blazers.dyproxy.dynamicproxy.util.CustomDynamicProxyVersion1;
 import com.chuhui.blazers.dyproxy.dynamicproxy.util.CustomDynamicProxyVersion2;
 import com.chuhui.blazers.dyproxy.dynamicproxy.util.CustomeInvokeHandler;
+import org.apache.commons.io.IOUtils;
 import org.junit.Test;
+import sun.misc.ProxyGenerator;
 
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.lang.reflect.Method;
 
 import static com.chuhui.blazers.dyproxy.dynamicproxy.jdkdynamicproxy.JdkDynamicProxy.generator;
@@ -31,11 +36,11 @@ public class DynamicProxyServiceTest {
     public void userJdkDynamicProxy() {
 
         DynamicProxyService dynamicProxyService = new DynamicProxyServiceImpl();
-
         DynamicProxyService setFiveForCountService = generator(dynamicProxyService, new SetFiveForCount(dynamicProxyService));
+        System.err.println(setFiveForCountService);
         setFiveForCountService.printParams("cyzi", -1);
-//        DynamicProxyService subtractTenService = generator(dynamicProxyService, new SubtractTen(dynamicProxyService));
-//        subtractTenService.printParams("cyzi", 100);
+        DynamicProxyService subtractTenService = generator(dynamicProxyService, new SubtractTen(dynamicProxyService));
+        subtractTenService.printParams("cyzi", 100);
 
     }
 
@@ -55,6 +60,29 @@ public class DynamicProxyServiceTest {
         service.printParams("cyzi", 10);
     }
 
+
+    /**
+     * 验证jdk动态代理产生的代理类对象
+     */
+    @Test
+    public void getJdkDynamicProxyBinaryStream() {
+
+        byte[] bytes = ProxyGenerator.generateProxyClass(
+                "customProxyName", new Class[]{DynamicProxyService.class}, 49);
+
+        try {
+            FileOutputStream fos = new FileOutputStream("D:\\customProxyName.class");
+            fos.write(bytes);
+            fos.flush();
+            fos.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
     /**
      * CustomeInvokeHandler的实现类,这里我们可以定义自己的处理逻辑
      */
@@ -65,6 +93,7 @@ public class DynamicProxyServiceTest {
         public CutomeInvokeHandlerImpl(Object target) {
             this.target = target;
         }
+
         @Override
         public Object invoke(Method method, Object[] args) throws Throwable {
             System.err.println("自己的逻辑");
