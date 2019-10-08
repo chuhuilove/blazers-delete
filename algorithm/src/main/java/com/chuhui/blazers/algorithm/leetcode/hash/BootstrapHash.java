56738e218e5de21be823059351d1a09d5868d161
chu-hui-love <lovehui131125@gmail.com>
2019-10-09 07:39:30 +0800

    LeetCode hash

---
+++ algorithm/src/main/java/com/chuhui/blazers/algorithm/leetcode/hash/BootstrapHash.java
@@ -0,0 +1,12 @@
+package com.chuhui.blazers.algorithm.leetcode.hash;
+
+/**
+ * BootstrapHash
+ * <p>
+ * //TODO description
+ *
+ * @author: 纯阳子
+ * @date: 2019/10/9
+ */
+public class BootstrapHash {
+}
---
+++ algorithm/src/main/java/com/chuhui/blazers/algorithm/leetcode/hash/SolutionHash.java
@@ -0,0 +1,55 @@
+package com.chuhui.blazers.algorithm.leetcode.hash;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * SolutionHash
+ * <p>
+ * //TODO description
+ *
+ * @author: 纯阳子
+ * @date: 2019/10/9
+ */
+public class SolutionHash {
+
+    /**
+     * 1 两数之和
+     * @param nums 给定数组
+     * @param target 目标值
+     * @return 返回数组
+     */
+    public int[] twoSum(int[] nums, int target) {
+
+        int [] res=new int[2];
+
+        // 差,原数字
+        Map<Integer,Integer> map=new HashMap<>();
+
+
+        for(int num:nums){
+
+            
+
+
+
+
+
+
+
+
+        }
+
+
+
+
+
+        return res;
+
+
+
+
+
+    }
+
+}
